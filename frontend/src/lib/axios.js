b2b84ee30208f8ebdd2f56cf1201418c8c498b59
Anuj1212G <157905556+Anuj1212G@users.noreply.github.com>
2025-07-11 18:34:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatsphere-s6h3.onrender.com/api" : "/api";
+const BASE_URL ="https://chatsphere-s6h3.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
